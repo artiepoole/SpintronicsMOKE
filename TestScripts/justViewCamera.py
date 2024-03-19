a991d0bbfa53e582424f066e3b8f4dbdd12b37fb
Artie Poole <stuart.f.poole@gmail.com>
2024-03-19 17:07:48 +0000

    Renamed the camera test scripts to avoid confusion

--- TestScripts/justViewCamera.py
+++ TestScripts/justViewCamera.py
@@ -1,37 +1,34 @@
 from pylablib.devices import DCAM
+import numpy as np
 import time
 import cv2
-from WrapperClasses.LampController import LampController
+
 
 cam = DCAM.DCAMCamera(idx=0)
 cam.set_attribute_value("EXPOSURE TIME", 0.05)
 height, width = cam.get_detector_size()
 cam.set_roi(hbin=2, vbin=2)
 
-fullscreen = True
-
 stream_window = 'HamamatsuView'
-if fullscreen:
-    window_width = width // 2
-    window_height = height // 2
-    cv2.namedWindow(
-        stream_window,
-        flags=(cv2.WINDOW_NORMAL | cv2.WINDOW_GUI_NORMAL | cv2.WINDOW_FREERATIO))
-    cv2.setWindowProperty(stream_window, cv2.WND_PROP_TOPMOST, 1.0)
-    cv2.setWindowProperty(stream_window, cv2.WND_PROP_FULLSCREEN, 1.0)
-    cv2.resizeWindow(
-        stream_window,
-        window_width,
-        window_height)
-    cv2.moveWindow(
-        stream_window,
-        0,
-        0)
+window_width = width//2
+window_height = height//2
+cv2.namedWindow(
+    stream_window,
+    flags=(cv2.WINDOW_NORMAL | cv2.WINDOW_GUI_NORMAL | cv2.WINDOW_FREERATIO))
+cv2.setWindowProperty(stream_window, cv2.WND_PROP_TOPMOST, 1.0)
+cv2.setWindowProperty(stream_window, cv2.WND_PROP_FULLSCREEN, 1.0)
+cv2.resizeWindow(
+    stream_window,
+    window_width,
+    window_height)
+cv2.moveWindow(
+    stream_window,
+    0,
+    0)
 
-lampController = LampController()
-lampController.enable_left()
 
-running = True  # press esc to close
+start_time = time.time()
+running = True
 while running:
     frame = cam.snap()
     cv2.imshow(stream_window, frame)
@@ -40,7 +37,6 @@ while running:
         running = False
 
 cam.close()
-lampController.disable_all()
-time.sleep(0.2)
-lampController.close()
 cv2.destroyAllWindows()
+
+
---
+++ TestScripts/singleLightViewCamera.py
@@ -0,0 +1,46 @@
+from pylablib.devices import DCAM
+import time
+import cv2
+from WrapperClasses.LampController import LampController
+
+cam = DCAM.DCAMCamera(idx=0)
+cam.set_attribute_value("EXPOSURE TIME", 0.05)
+height, width = cam.get_detector_size()
+cam.set_roi(hbin=2, vbin=2)
+
+fullscreen = True
+
+stream_window = 'HamamatsuView'
+if fullscreen:
+    window_width = width // 2
+    window_height = height // 2
+    cv2.namedWindow(
+        stream_window,
+        flags=(cv2.WINDOW_NORMAL | cv2.WINDOW_GUI_NORMAL | cv2.WINDOW_FREERATIO))
+    cv2.setWindowProperty(stream_window, cv2.WND_PROP_TOPMOST, 1.0)
+    cv2.setWindowProperty(stream_window, cv2.WND_PROP_FULLSCREEN, 1.0)
+    cv2.resizeWindow(
+        stream_window,
+        window_width,
+        window_height)
+    cv2.moveWindow(
+        stream_window,
+        0,
+        0)
+
+lampController = LampController()
+lampController.enable_left()
+
+running = True  # press esc to close
+while running:
+    frame = cam.snap()
+    cv2.imshow(stream_window, frame)
+    k = cv2.waitKey(10)
+    if k == 27:
+        running = False
+
+cam.close()
+lampController.disable_all()
+time.sleep(0.2)
+lampController.close()
+cv2.destroyAllWindows()
