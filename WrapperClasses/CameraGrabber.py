44a6cd90d2a56919139df64009a98c68b2335ff2
Artie Poole <stuart.f.poole@gmail.com>
2024-04-05 19:20:22 +0100

    Implemented mutex and implemented proper closing of the thread/camera.

--- WrapperClasses/CameraGrabber.py
+++ WrapperClasses/CameraGrabber.py
@@ -10,29 +10,43 @@ class CameraGrabber(QtCore.QObject):
     EXPOSURE_THIRTY_FPS = 1
     frame_from_camera_ready_signal = QtCore.pyqtSignal(np.ndarray)
     frame_stack_from_camera_ready_signal = QtCore.pyqtSignal(np.ndarray)
+    quit_ready = QtCore.pyqtSignal()
     cam = DCAM.DCAMCamera(idx=0)
     exposure_time = 0.05
     cam.set_attribute_value("EXPOSURE TIME", exposure_time)
     # self.print_to_queue(self.cam.info)
     cam.set_roi(hbin=2, vbin=2)
     running = False
+    closing = False
     averages = 16
     averaging = False
+    mutex = QtCore.QMutex()
+
 
     def start_live_single_frame(self):
         self.cam.setup_acquisition()
         self.cam.start_acquisition()
+        self.mutex.lock()
         self.running = True
+        self.mutex.unlock()
         self.averaging = False
         while self.running:
             frame = self.cam.read_newest_image()
             if frame is not None:
                 self.frame_from_camera_ready_signal.emit(frame)
         self.cam.stop_acquisition()
+        print("Camera stopped")
+        if self.closing:
+            print("Camera closing")
+            self.cam.close()
+            self.quit_ready.emit()
+
 
     def start_averaging(self):
+        self.mutex.lock()
         self.running = True
         self.averaging = True
+        self.mutex.unlock()
         self.cam.setup_acquisition()
         self.cam.start_acquisition()
         frames = []
@@ -49,6 +63,11 @@ class CameraGrabber(QtCore.QObject):
                 self.frame_stack_from_camera_ready_signal.emit(np.array(frames))
                 i = i + 1
         self.cam.stop_acquisition()
+        print("Camera stopped")
+        if self.closing:
+            print("Camera closing")
+            self.cam.close()
+            self.quit_ready.emit()
 
     def set_exposure_time(self, exposure_time_idx):
         self.cam.stop_acquisition()
@@ -82,6 +101,6 @@ if __name__ == "__main__":
     import skimage
 
     camera_grabber = CameraGrabber(None)
-    frames = camera_grabber.grab_n_frames(500)
+    frames = camera_grabber.grab_n_frames(10)
     cv2.imshow('avg', skimage.exposure.equalize_hist(np.mean(np.array(frames), axis=0)))
     cv2.waitKey(0)
