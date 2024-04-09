a66d83760a0de4347dc96e4b187431851628cbe6
Artie Poole <stuart.f.poole@gmail.com>
2024-04-09 15:41:12 +0100

    Solved the problem of configuring the trigger mode to be how we want. Trigger signal is sent on P0/L4 as a falling edge trigger.

--- WrapperClasses/CameraGrabber.py
+++ WrapperClasses/CameraGrabber.py
@@ -9,10 +9,12 @@ class CameraGrabber(QtCore.QObject):
     EXPOSURE_TWENTY_FPS = 0
     EXPOSURE_THIRTY_FPS = 1
     frame_from_camera_ready_signal = QtCore.pyqtSignal(np.ndarray)
+    difference_frame_ready = QtCore.pyqtSignal((np.ndarray, np.ndarray))
     frame_stack_from_camera_ready_signal = QtCore.pyqtSignal(np.ndarray)
     quit_ready = QtCore.pyqtSignal()
     cam = DCAM.DCAMCamera(idx=0)
     exposure_time = 0.05
+    cam.set_trigger_mode('int')
     cam.set_attribute_value("EXPOSURE TIME", exposure_time)
     # self.print_to_queue(self.cam.info)
     cam.set_roi(hbin=2, vbin=2)
@@ -20,16 +22,18 @@ class CameraGrabber(QtCore.QObject):
     closing = False
     averages = 16
     averaging = False
+    difference_mode = False
     mutex = QtCore.QMutex()
 
-
     def start_live_single_frame(self):
+        self.set_trigger_mode('int')
         self.cam.setup_acquisition()
         self.cam.start_acquisition()
         self.mutex.lock()
         self.running = True
-        self.mutex.unlock()
         self.averaging = False
+        self.difference_mode = False
+        self.mutex.unlock()
         while self.running:
             frame = self.cam.read_newest_image()
             if frame is not None:
@@ -41,12 +45,14 @@ class CameraGrabber(QtCore.QObject):
             self.cam.close()
             self.quit_ready.emit()
 
-
     def start_averaging(self):
+
         self.mutex.lock()
         self.running = True
         self.averaging = True
+        self.difference_mode = False
         self.mutex.unlock()
+        self.cam.set_trigger_mode('int')
         self.cam.setup_acquisition()
         self.cam.start_acquisition()
         frames = []
@@ -69,6 +75,42 @@ class CameraGrabber(QtCore.QObject):
             self.cam.close()
             self.quit_ready.emit()
 
+    def start_difference_mode_single(self):
+        self.mutex.lock()
+        self.running = True
+        self.averaging = False
+        self.difference_mode = True
+        self.mutex.unlock()
+        need_pos = True
+        self.set_trigger_mode()
+        self.cam.setup_acquisition()
+        self.cam.start_acquisition()
+        while self.running:
+            frame = self.cam.read_newest_image()
+            if frame is not None:
+                if need_pos:
+                    frame_a = frame.copy()
+                    need_pos = False
+                else:
+                    frame_b = frame.copy()
+                    need_pos = True
+                    self.frame_from_camera_ready_signal.emit((frame_a, frame_b))
+        self.cam.stop_acquisition()
+        print("Camera stopped")
+        if self.closing:
+            print("Camera closing")
+            self.cam.close()
+            self.quit_ready.emit()
+
+    def start_difference_mode_averaging(self):
+        self.mutex.lock()
+        self.running = True
+        self.averaging = False
+        self.difference_mode = True
+        self.mutex.unlock()
+        need_pos = True
+        self.set_trigger_mode('ext')
+
     def set_exposure_time(self, exposure_time_idx):
         self.cam.stop_acquisition()
         match exposure_time_idx:
@@ -85,6 +127,17 @@ class CameraGrabber(QtCore.QObject):
         else:
             self.start_live_single_frame()
 
+    def set_trigger_mode(self):
+        if self.difference_mode:
+            camera_grabber.cam.set_attribute_value('TRIGGER SOURCE', 2)  # External
+            camera_grabber.cam.set_attribute_value('TRIGGER MODE', 1)  # Normal (as opposed to "start")
+            camera_grabber.cam.set_attribute_value('TRIGGER ACTIVE', 1)  # Edge
+            camera_grabber.cam.set_attribute_value('TRIGGER POLARITY', 1)  # Falling
+            camera_grabber.cam.set_attribute_value('TRIGGER TIMES', 1)  # One frame per trigger signal
+        else:
+            self.cam.set_trigger_mode('int')
+            print("setting trigger mode to int")
+
     def snap(self):
         self.frame_from_camera_ready_signal.emit(self.cam.snap())
 
@@ -99,8 +152,8 @@ class CameraGrabber(QtCore.QObject):
 if __name__ == "__main__":
     import cv2
     import skimage
+    import numpy as np
 
     camera_grabber = CameraGrabber(None)
-    frames = camera_grabber.grab_n_frames(10)
-    cv2.imshow('avg', skimage.exposure.equalize_hist(np.mean(np.array(frames), axis=0)))
-    cv2.waitKey(0)
+
+
