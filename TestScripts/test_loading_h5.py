e1ca216395fc0af30a623e710bae0cff6511c784
Artie Poole <stuart.f.poole@gmail.com>
2024-04-16 11:14:03 +0100

    playing with loading

--- TestScripts/test_loading_h5.py
+++ TestScripts/test_loading_h5.py
@@ -6,14 +6,15 @@ import numpy as np
 
 file = filedialog.askopenfilename()
 meta_data = pd.read_hdf(file, 'meta_data')
+print(meta_data)
 contents = meta_data.contents[0]
+print(contents)
 frames = {}
 for item in contents:
-    frames[item] = pd.read_hdf(file, item)
-myavg = frames["frame_avg"].values
-mybkg = frames["background"].values
-cv2.imshow('averaged', (exposure.equalize_hist(myavg) * 65535).astype(np.uint16))
-cv2.imshow('subtracted', (exposure.equalize_hist(myavg - mybkg) * 65535).astype(np.uint16))
-cv2.waitKey(0)
-
-
+    if "stack" in item:
+        frames[item] = pd.read_hdf(file, item)
+# myavg = frames["frame_avg"].values
+# mybkg = frames["background"].values
+# cv2.imshow('averaged', (exposure.equalize_hist(myavg) * 65535).astype(np.uint16))
+# cv2.imshow('subtracted', (exposure.equalize_hist(myavg - mybkg) * 65535).astype(np.uint16))
+# cv2.waitKey(0)
