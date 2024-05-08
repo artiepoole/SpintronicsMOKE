6780817a14d057c0d5e2bd4c8354c72765a7d68b
Artie Poole <stuart.f.poole@gmail.com>
2024-05-08 18:29:27 +0100

    Refactored file name to avoid pycharm test mode activating.
    Just displays all measured data by simple dividing.

--- TestScripts/example_loading_h5.py
+++ TestScripts/example_loading_h5.py
@@ -4,15 +4,25 @@ import cv2
 from skimage import exposure
 import numpy as np
 
+
+
+
+
 file = filedialog.askopenfilename()
 meta_data = pd.read_hdf(file, 'meta_data')
 print(meta_data)
 contents = meta_data.contents[0]
 print(contents)
 frames = {}
+
+
+
 for item in contents:
-    if "stack" in item:
-        frames[item] = pd.read_hdf(file, item)
+    data = pd.read_hdf(file, item).values
+    cv2.imshow(item, data/np.amax(data))
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 # myavg = frames["frame_avg"].values
 # mybkg = frames["background"].values
 # cv2.imshow('averaged', (exposure.equalize_hist(myavg) * 65535).astype(np.uint16))
